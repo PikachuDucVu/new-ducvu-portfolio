0c80ecc496e22fc5d589bbaeacf653930e128740
PikachuDucVu <ducvubn1@gmail.com>
2024-07-25 03:11:31 +0700

    responsive

--- components/About.tsx
+++ components/About.tsx
@@ -16,12 +16,12 @@ const About = () => {
     <div className="bg-[#121121] pb-12 pt-16 md:pt-32" id="about">
       <div className="grid grid-cols-1 md:grid-cols-2 w-[80%] mx-auto gap-12 items-center">
         <div>
-          <h1 className="text-[24px]  font-bold uppercase text-[#55e6a5] mb-[1rem]">
+          <h1 className="text-[24px] font-bold uppercase text-[#55e6a5] mb-[1rem]">
             About Me
           </h1>
           <div className="mb-12 flex items-center md:space-x-10">
             <span className="w-[100px] hidden md:block h-5 bg-slate-400 rounded-sm"></span>
-            <p className="text-[19px] text-slate-300 w-[55%] ">
+            <p className="text-[19px] text-slate-300 md:w-[55%] w-full text">
               Currently, I am working as a Game and Frontend Developer at
               TEKMATE Software Solutions Ltd, responsible for developing and
               maintaining game interfaces and web applications. Additionally, I
@@ -31,7 +31,7 @@ const About = () => {
             </p>
           </div>
 
-          <div className="flex gap-3">
+          <div className="hidden lg:flex gap-3">
             <Link
               to="social"
               smooth
@@ -58,6 +58,25 @@ const About = () => {
             className="relative z-[11] w-full h-full object-contain"
             alt=""
           />
+        </div>
+
+        <div className="lg:hidden flex gap-3 justify-center items-center">
+          <Link
+            to="social"
+            smooth
+            className="px-8 hover:bg-yellow-400 transition-all duration-200 py-4 text-[18px] font-bold uppercase bg-[#3498db]  text-black flex items-center space-x-2"
+          >
+            <p>Social</p>
+            <ShareIcon className="h-6 w-6 " />
+          </Link>
+
+          <button
+            className="px-8 hover:bg-yellow-400 transition-all duration-200 py-4 text-[18px] font-bold uppercase bg-[#55e6a5]  text-black flex items-center space-x-2"
+            onClick={handleDownload}
+          >
+            <p>CV</p>
+            <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black " />
+          </button>
         </div>
       </div>
       s
